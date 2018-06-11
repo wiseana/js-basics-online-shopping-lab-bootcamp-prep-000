304d2d19999333118f0a012b7f0a343f4b95c481
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-11 03:03:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (`${credit}`==="undefined") {return "Sorry, we don't have your credit card on file."} else {return `Your total cost is $${total()}, which will be charged to the card ${credit}.`}
+  if (`${cardNumber}`==="undefined") {return "Sorry, we don't have your credit card on file."} else {return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`}
   }
